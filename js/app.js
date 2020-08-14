c7ac52059c8028c5d08b9ccd25dd3a91dd0db364
Sasha Beep <thecomicz@gmail.com>
2020-08-14 14:19:29 +0300

    Update app.js
    Dropped navfixer function regarding smartmenus plugin

--- js/app.js
+++ js/app.js
@@ -57,45 +57,7 @@ $(document).ready(function () {
 		.not('[href*="' + window.location.host + '"]')
 		.attr('rel', 'noopener noreferrer')
 		.attr('target', '_blank');
-
-
-	//navfixer
-	navfixer();
 	
-});
-
-function navfixer() {
-	const $dropdown = $(".dropdown");
-	const $dropdownToggle = $(".dropdown-toggle");
-	const $dropdownMenu = $(".dropdown-menu");
-	const showClass = "show";
-	if (this.matchMedia("(min-width: 768px)").matches) {
-		$dropdown.hover(
-			function () {
-				const $this = $(this);
-				$this.addClass(showClass);
-				$this.find($dropdownToggle).attr("aria-expanded", "true");
-				$this.find($dropdownMenu).addClass(showClass);
-			},
-			function () {
-				const $this = $(this);
-				$this.removeClass(showClass);
-				$this.find($dropdownToggle).attr("aria-expanded", "false");
-				$this.find($dropdownMenu).removeClass(showClass);
-			}
-		);
-		$dropdownToggle.on("click", function () {
-			window.location.href = $(this).attr("href");
-			return false;
-		});
-	} else {
-		$dropdown.off("mouseenter mouseleave");
-		$dropdownToggle.off("click");
-	}
-}
-
-$(window).on("load resize orientationchange", function () {
-	navfixer()
 });
 
 $(window).on('load',function(){
